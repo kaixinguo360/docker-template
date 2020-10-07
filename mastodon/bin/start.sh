51dca30a36e8266411671df70c9cab1cecc3d585
Kaixinguo <kaixinguo@kaixinguo.site>
2020-10-07 14:16:12 +0800

    Optimize config of some templates

--- mastodon/bin/start.sh
+++ mastodon/bin/start.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/..
 
 DEPLOY_NODE_WEB="node.hostname==w.<secret:HOST_NAME>" \
 DEPLOY_NODE_REDIS="node.hostname==w.<secret:HOST_NAME>" \
-DEPLOY_NODE_DB="node.hostname==w.<secret:HOST_NAME>" \
-DEPLOY_NODE_STREAMING="node.hostname==w.<secret:HOST_NAME>" \
-DEPLOY_NODE_SIDEKIQ="node.hostname==p.<secret:HOST_NAME>" \
+DEPLOY_NODE_DB="node.hostname==m.<secret:HOST_NAME>" \
+DEPLOY_NODE_STREAMING="node.hostname==m.<secret:HOST_NAME>" \
+DEPLOY_NODE_SIDEKIQ="node.hostname==m.<secret:HOST_NAME>" \
 docker stack deploy -c docker-compose.yml mastodon
