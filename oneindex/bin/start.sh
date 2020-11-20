78c2000e074867b87127cde8cacdd5b2d11adc49
Kaixinguo <kaixinguo@kaixinguo.site>
2020-11-20 17:54:51 +0000

    Update template: oneindex

--- oneindex/bin/start.sh
+++ oneindex/bin/start.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname $0)/..
 
-DEPLOY_NODE_WEB="node.hostname==w.<secret:HOST_NAME>" \
+DEPLOY_NODE_WEB="node.hostname==p.<secret:HOST_NAME>" \
 docker stack deploy -c docker-compose.yml oneindex
