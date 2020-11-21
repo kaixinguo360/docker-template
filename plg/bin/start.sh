42a3485b90e06e11f071826095e1185d0936b377
Kaixinguo <kaixinguo@kaixinguo.site>
2020-11-21 17:37:28 +0000

    Update template: plg

--- plg/bin/start.sh
+++ plg/bin/start.sh
@@ -2,5 +2,7 @@
 
 cd $(dirname $0)/..
 
-DEPLOY_NODE_WEB="node.hostname==p.<secret:HOST_NAME>" \
+DEPLOY_NODE_GRAFANA="node.hostname==p.<secret:HOST_NAME>" \
+DEPLOY_NODE_LOKI="node.hostname==p.<secret:HOST_NAME>" \
+DEPLOY_NODE_INFLUXDB="node.hostname==m.<secret:HOST_NAME>" \
 docker stack deploy -c docker-compose.yml plg
