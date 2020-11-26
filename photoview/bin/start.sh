79c70efca53b9ee6fe26057625210d23ecb6bdf8
Kaixinguo <kaixinguo@kaixinguo.site>
2020-11-26 06:16:39 +0000

    Optimize photoview

--- photoview/bin/start.sh
+++ photoview/bin/start.sh
@@ -2,5 +2,6 @@
 
 cd $(dirname $0)/..
 
-DEPLOY_NODE_WEB="node.hostname==p.<secret:HOST_NAME>" \
+DEPLOY_NODE_WEB="node.hostname==w.<secret:HOST_NAME>" \
+DEPLOY_NODE_DB="node.hostname==w.<secret:HOST_NAME>" \
 docker stack deploy -c docker-compose.yml photoview
