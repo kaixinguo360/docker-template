3dc4bf72f7bbdab51d9188d9f4c3414706fbb886
Kaixinguo <kaixinguo@kaixinguo.site>
2021-01-31 15:13:55 +0800

    Update all templates: optimize bin/info.sh

--- dashmachine/bin/info.sh
+++ dashmachine/bin/info.sh
@@ -2,10 +2,14 @@
 
 . $(dirname $0)/lib.sh
 
-cat << 'HERE'
-# Use `info.sh > deploy.env` to save current config as default profile
-# Use `info.sh > deploy-<profile>.env` to save current config as a new profile
-# Use `start.sh <profile>` to deploy stack with saved profile
+cat << HERE
+# Use \`info.sh > deploy.env\` to save current config as default profile
+# Use \`info.sh > deploy-<profile>.env\` to save current config as a new profile
+# Use \`start.sh <profile>\` to deploy stack with saved profile
+#
+# The following are all features which can be used in the DEPLOY_ENABLED_FEATURE
+$(sed -nE 's/.*#[!:]([^ |]*).*/# - \1/p' docker-compose.yml | sort -u)
+#
 HERE
 
 grep '${DEPLOY_[^}]*}\|$DEPLOY_[A-Z_a-z0-9]*' \
