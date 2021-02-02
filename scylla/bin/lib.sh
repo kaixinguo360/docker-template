92f2e68143915f1fff332278cde1eed266ee74be
Kaixinguo <kaixinguo@kaixinguo.site>
2021-02-02 20:44:05 +0800

    Update all templates: optimize bin/lib.sh

--- scylla/bin/lib.sh
+++ scylla/bin/lib.sh
@@ -11,8 +11,19 @@ cd $(dirname $0)/..
 # Get Default Stack Name
 export DEPLOY_STACK_NAME="$(basename $(realpath $(pwd)))"
 
+# Get Profile Name
+if [ -n "$1" ]; then
+    if [ -n "$(echo "$1"|grep '^.*deploy.*.env$')" ]; then
+        PROFILE=$(printf '%s' "$1"|sed -nE 's/^.*deploy-([a-zA-Z0-9]*)\.env$/\1/p')
+    elif [ -n "$(echo "$1"|grep '^.*docker-compose.*.yml$')" ]; then
+        PROFILE=$(printf '%s' "$1"|sed -nE 's/^.*docker-compose-([a-zA-Z0-9]*)\.yml$/\1/p')
+    else
+        PROFILE="$1"
+    fi
+    shift
+fi
+
 # Load Profile
-[ -n "$1" ] && PROFILE="$1" && shift
 set -o allexport
 [ -z "$PROFILE" -a -f ./deploy.env ] && . ./deploy.env
 [ -n "$PROFILE" -a -f "./deploy-$PROFILE.env" ] && . "./deploy-$PROFILE.env"
