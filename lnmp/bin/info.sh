afe5a67732f2ed173d9dbc816503a8e6222df87e
Kaixinguo <kaixinguo@kaixinguo.site>
2021-02-14 23:58:26 +0800

    Add template: lnmp

---
+++ lnmp/bin/info.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. $(dirname $0)/lib.sh
+
+cat << HERE
+# Use \`info.sh > deploy.env\` to save current config as default profile
+# Use \`info.sh > deploy-<profile>.env\` to save current config as a new profile
+# Use \`start.sh <profile>\` to deploy stack with saved profile
+#
+# The following are all features which can be used in the DEPLOY_ENABLED_FEATURE
+$(sed -nE 's/.*#[!:]([^ |]*).*/# - \1/p' docker-compose.yml | sort -u)
+#
+HERE
+
+grep '${DEPLOY_[^}]*}\|$DEPLOY_[A-Z_a-z0-9]*' \
+    -r -o -h \
+    --exclude=info.sh \
+    ./bin/* ./config/* ./docker-compose*.yml \
+    2>/dev/null \
+    | sort -u \
+    | xargs -l1 -i sh -c 'echo $(echo \{}|grep -o "DEPLOY_[A-Z_a-z0-9]*")=\"{}\"' \
+    | sort -u
+
---
+++ lnmp/src/run.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+replace() {
+    local target source
+    source="$(realpath "$1")"
+    target="$2"
+    printf 'Replace %s -> %s\n' "$target" "$source"
+    rm -rf "$target"
+    ln -s "$source" "$target"
+}
+
+cd /var/www
+
+# Config #
+
+[ -f './init.sh' ] \
+    && printf 'Init script: %s' "$(realpath ./init.sh)" \
+    && . ./init.sh
+
+if [ -n "$DEBUG" ]; then
+    printf 'DEBUG=true\n'
+    mv "$PHP_INI_DIR/php-dev.ini" "$PHP_INI_DIR/php.ini"
+else
+    printf 'DEBUG=false\n'
+    mv "$PHP_INI_DIR/php-prod.ini" "$PHP_INI_DIR/php.ini"
+fi
+
+[ -f './nginx.conf' ] \
+    && replace ./nginx.conf /etc/nginx/conf.d/default.conf
+[ -d './nginx.conf.d' ] \
+    && replace ./nginx.conf.d /etc/nginx/conf.d
+[ -d './nginx.ext' ] \
+    && replace ./nginx.ext /etc/nginx/conf.ext
+[ -d './nginx' ] \
+    && replace ./nginx /etc/nginx
+
+[ -f './php.ini' ] \
+    && replace ./php.ini "$PHP_INI_DIR/conf.d/default.ini"
+[ -d './php.conf.d' ] \
+    && replace ./php.conf.d "$PHP_INI_DIR/conf.d"
+[ -d './php' ] \
+    && replace ./php "$PHP_INI_DIR"
+
+# Run #
+
+php-fpm &
+
+if [ -n "$DB_HOST" ]; then
+
+    printf 'DB_HOST=%s\nDB_PORT=%s\nDB_LOCAL_PORT=%s\n' \
+        "$DB_HOST" "$DB_PORT" "$DB_LOCAL_PORT"
+
+    printf 'Socat %s -> %s\n' "/run/mysqld/mysqld.sock" "$DB_HOST:$DB_PORT"
+    mkdir -p /run/mysqld/
+    socat UNIX-LISTEN:/run/mysqld/mysqld.sock,fork,mode=777 TCP:$DB_HOST:$DB_PORT &
+
+    if [ -n "$DB_LOCAL_PORT" ]; then
+        printf 'Socat %s -> %s\n' "127.0.0.1:$DB_LOCAL_PORT" "$DB_HOST:$DB_PORT"
+        socat TCP-LISTEN:$DB_LOCAL_PORT,fork TCP:$DB_HOST:$DB_PORT &
+    fi
+fi
+
+nginx -g "daemon off;"
+
