cb3258c14b0df78a87d60381c21efb1b8d4f7e95
Kaixinguo <kaixinguo@kaixinguo.site>
2021-02-27 14:14:17 +0800

    Update all templates: optimize bin/info.sh and bin/config.sh

--- robotsdisallow/bin/config.sh
+++ robotsdisallow/bin/config.sh
@@ -4,7 +4,7 @@
 
 cat docker-compose.yml | {
     if [ -n "$DEPLOY_ENABLED_FEATURE" ]; then
-        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\|)?(.*)$/\\3/g" | \
+        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\||\s+)(.*)$/\\3/g" | \
         sed -E "s/^(.*)\s*#!($DEPLOY_ENABLED_FEATURE)\s*$/#\\1/g"
     else
         cat
