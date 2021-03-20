e785bc67b7ef0b0128028b4469099c61620000e0
Kaixinguo <kaixinguo@kaixinguo.site>
2021-03-20 09:13:40 +0800

    Update some templates: better bin/init.sh

--- jenkins/bin/init.sh
+++ jenkins/bin/init.sh
@@ -2,7 +2,25 @@
 
 . $(dirname $0)/lib.sh
 
-printf "Initing... " && {
-    sudo chown -R 1000:1000 ./var/data
-} && echo 'done.'
+printf 'Preparing tmp volume... '
+docker volume rm -f tmp_volume >/dev/null || exit 1
+docker volume create tmp_volume \
+    --driver local \
+    -o "type=nfs" \
+    -o "o=addr=$DEPLOY_DATA_HOST,nolock,soft,rw" \
+    -o "device=:$DEPLOY_DATA_ROOT/$DEPLOY_STACK_NAME" \
+    >/dev/null \
+    && printf 'ok\n' \
+    || exit 1
+
+printf 'Changing owner of data dir.. '
+docker run --rm -v tmp_volume:/data alpine:3 \
+    sh -c "chown -R 1000:1000 /data" \
+    && printf 'ok\n' \
+    || exit 1
+
+printf 'Removing tmp volume... '
+docker volume rm -f tmp_volume >/dev/null \
+    && printf 'ok\n' \
+    || exit 1
 
--- rocket/bin/init.sh
+++ rocket/bin/init.sh
@@ -2,17 +2,26 @@
 
 . $(dirname $0)/lib.sh
 
-printf "Initing... " && {
-    docker run --network rocket_internal_network --rm mongo:4.0 bash << 'HERE'
-    for i in `seq 1 30`; do
-      mongo db/rocketchat --eval \"
-        rs.initiate({
-          _id: 'rs0',
-          members: [ { _id: 0, host: 'localhost:27017' } ]})\" &&
-      s=$$? && break || s=$$?;
-      echo \"Tried $$i times. Waiting 5 secs...\";
-      sleep 5;
-    done; (exit $$s)
-HERE
-} && echo 'done.'
+printf 'Starting stack %s...\n' "$DEPLOY_STACK_NAME"
+./bin/start.sh $PROFILE 2>&1 | sed 's/^/  /g'
+printf 'ok\n'
+
+printf 'Initing db...\n'
+docker run \
+    --network ${DEPLOY_STACK_NAME}_internal_network \
+    --rm -i mongo:4.0 bash -c "
+      for i in \`seq 1 30\`; do
+        mongo db/rocketchat --eval \"
+          rs.initiate({
+            _id: 'rs0',
+            members: [ { _id: 0, host: 'localhost:27017' } ]})\" &&
+        s=\$? && break || s=\$?;
+        echo \"Tried \$i times. Waiting 5 secs...\";
+        sleep 5;
+      done; (exit \$s)" 2>&1 | sed 's/^/  /g'
+printf 'ok\n'
+
+printf 'Stopping stack %s...\n' "$DEPLOY_STACK_NAME"
+./bin/stop.sh $PROFILE 2>&1 | sed 's/^/  /g'
+printf 'ok\n'
 
