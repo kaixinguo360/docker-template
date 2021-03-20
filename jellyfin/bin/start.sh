14bc7894678b3d0ff5b98e7ca4fc8f77208cbdbc
Kaixinguo <kaixinguo@kaixinguo.site>
2021-03-20 09:39:56 +0800

    Update all templates: better bin/start.sh

--- jellyfin/bin/start.sh
+++ jellyfin/bin/start.sh
@@ -2,9 +2,11 @@
 
 . $(dirname $0)/lib.sh
 
-./bin/config.sh | {
-    if [ -f "./docker-compose-$PROFILE.yml" ]; then
-        docker stack deploy -c - -c "./docker-compose-$PROFILE.yml" $DEPLOY_STACK_NAME
+./bin/config.sh $PROFILE | {
+    if [ -z "$PROFILE" -a -f "./deploy-compose.yml" ]; then
+        docker stack deploy -c - -c "./deploy-compose.yml" $DEPLOY_STACK_NAME
+    elif [ -n "$PROFILE" -a -f "./deploy-compose-$PROFILE.yml" ]; then
+        docker stack deploy -c - -c "./deploy-compose-$PROFILE.yml" $DEPLOY_STACK_NAME
     else
         docker stack deploy -c - $DEPLOY_STACK_NAME
     fi
