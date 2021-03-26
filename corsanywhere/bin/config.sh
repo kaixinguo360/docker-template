77dbf45c5fa3c444ee209c5443ee3b3815d5ce2f
Kaixinguo <kaixinguo@kaixinguo.site>
2021-03-26 15:08:58 +0800

    Add template: corsanywhere

---
+++ corsanywhere/bin/config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. $(dirname $0)/lib.sh
+
+cat docker-compose.yml | {
+    if [ -n "$DEPLOY_ENABLED_FEATURE" ]; then
+        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\||\s+)(.*)$/\\3/g" | \
+        sed -E "s/^(.*)\s*#!($DEPLOY_ENABLED_FEATURE)\s*$/#\\1/g"
+    else
+        cat
+    fi
+}
+
