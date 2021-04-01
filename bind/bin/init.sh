95d235d6611b0f852604135c9325d2f678b75221
Kaixinguo <kaixinguo@kaixinguo.site>
2021-04-01 15:45:35 +0800

    Add template: bind

---
+++ bind/bin/init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. $(dirname $0)/lib.sh
+
+cat << HERE
+
+--------
+ README
+--------
+
+Default User: root/password
+
+You should change the default password for root user as soon as possible!
+
+Full documentation at: <https://hub.docker.com/r/sameersbn/bind>
+
+HERE
+
