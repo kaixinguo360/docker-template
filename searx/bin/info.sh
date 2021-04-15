9bd8f46a3b042b2b009fce163aa68233f2905c17
Kaixinguo <kaixinguo@kaixinguo.site>
2021-04-15 11:15:54 +0800

    Update all templates: better doc in bin/info.sh

--- searx/bin/info.sh
+++ searx/bin/info.sh
@@ -5,9 +5,13 @@
 cat << HERE
 # Use \`info.sh > deploy.env\` to save current config as default profile
 # Use \`info.sh > deploy-<profile>.env\` to save current config as a new profile
-# Use \`start.sh <profile>\` to deploy stack with saved profile
+# Use \`deploy.sh <profile>\` to deploy a new stack with specific profile
+# Use \`start.sh <profile>\` and \`stop.sh <profile>\` to start/stop deployed stack
+# All <profile> arguments can be omitted or replaced with \`-\` to use default profile
 #
-# The followings are all features which can be used in the DEPLOY_ENABLED_FEATURE, separated by \`|\`
+# The followings are all optional features, which can be enabled by adding to the DEPLOY_ENABLED_FEATURE
+# environment variable. Separate multiple enabled features with \`|\`
+# Use \`grep -n '#.<feature-name>' ./docker-compose.yml\` to see the meaning of each feature
 $(sed -nE 's/.*#[!:]([^ |]*).*/# - \1/p' docker-compose.yml | sort -u)
 #
 HERE
