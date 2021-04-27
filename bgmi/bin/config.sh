4898cea08ef69f8f4ab448788c22ba55c1df182e
Kaixinguo <kaixinguo@kaixinguo.site>
2021-04-27 16:14:00 +0800

    Update all templates: better bin/config.sh

--- bgmi/bin/config.sh
+++ bgmi/bin/config.sh
@@ -4,8 +4,9 @@
 
 cat docker-compose.yml | {
     if [ -n "$DEPLOY_ENABLED_FEATURE" ]; then
-        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\|)?(.*)$/\\3/g" | \
-        sed -E "s/^(.*)\s*#!($DEPLOY_ENABLED_FEATURE)\s*$/#\\1/g"
+        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\||\s+)(.*)$/\\3/g" | \
+        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\||\s+)(.*)$/\\3/g" | \
+        sed -E "s/^(.*)\s*#!($DEPLOY_ENABLED_FEATURE)\s*($|#!)/#\\1/g"
     else
         cat
     fi
--- komga/bin/config.sh
+++ komga/bin/config.sh
@@ -5,7 +5,8 @@
 cat docker-compose.yml | {
     if [ -n "$DEPLOY_ENABLED_FEATURE" ]; then
         sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\||\s+)(.*)$/\\3/g" | \
-        sed -E "s/^(.*)\s*#!($DEPLOY_ENABLED_FEATURE)\s*$/#\\1/g"
+        sed -E "s/^#:($DEPLOY_ENABLED_FEATURE)(\s*\||\s+)(.*)$/\\3/g" | \
+        sed -E "s/^(.*)\s*#!($DEPLOY_ENABLED_FEATURE)\s*($|#!)/#\\1/g"
     else
         cat
     fi
