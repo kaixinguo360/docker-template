7c44321aa58e8a6bbab0c42a8d52bbab9f502976
Kaixinguo <kaixinguo@kaixinguo.site>
2021-08-13 02:03:54 +0800

    Update template: hadoop, add HBase

--- hadoop/bin/run-hbase.sh
+++ hadoop/bin/run-hbase.sh
@@ -6,7 +6,7 @@ docker run \
     -v "${DEPLOY_LOCAL_MOUNT_POINT?Please set local mount point for tmp docker container}":/root \
     --env-file ${DEPLOY_CONFIG_HDFS:-config/hdfs.env} \
     --env-file ${DEPLOY_CONFIG_YARN:-config/yarn.env} \
-    --env-file ${DEPLOY_CONFIG_HIVE:-config/hive.env} \
+    --env-file ${DEPLOY_CONFIG_HBASE:-config/hbase.env} \
     --network ${DEPLOY_STACK_NAME}_internal_network \
-    --rm -it ${DEPLOY_IMAGE_HIVE_RUN:-bde2020/hive:2.3.2-postgresql-metastore} ${*:-bash}
+    --rm -it ${DEPLOY_IMAGE_HBASE_RUN:-bde2020/hbase-master:1.0.0-hbase1.2.6} ${*:-bash}
 
--- hadoop/bin/run-hdfs.sh
+++ hadoop/bin/run-hdfs.sh
@@ -5,8 +5,8 @@
 id=$(
   docker create \
     -v "${DEPLOY_LOCAL_MOUNT_POINT?Please set local mount point for tmp docker container}":/root \
-    --env-file ${DEPLOY_CONFIG_HADOOP:-config/hadoop.env} \
-    --rm -it ${DEPLOY_IMAGE_RUN_HDFS:-bde2020/hadoop-base:2.0.0-hadoop3.2.1-java8} ${*:-bash}
+    --env-file ${DEPLOY_CONFIG_HDFS:-config/hdfs.env} \
+    --rm -it ${DEPLOY_IMAGE_HDFS_RUN:-bde2020/hadoop-base:2.0.0-hadoop3.2.1-java8} ${*:-bash}
 )
 
 docker network connect external_network $id
