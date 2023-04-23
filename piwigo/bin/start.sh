5fe25ebcfcabb5bd8d76b99bf876440e7d4718d0
Kaixinguo <kaixinguo@kaixinguo.site>
2023-04-23 18:46:03 +0800

    Add template: piwigo

---
+++ piwigo/bin/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. $(dirname $0)/lib.sh
+
+./bin/config.sh $PROFILE | {
+    if [ -z "$PROFILE" -a -f "./deploy-compose.yml" ]; then
+        docker stack deploy -c - -c "./deploy-compose.yml" $DEPLOY_STACK_NAME
+    elif [ -n "$PROFILE" -a -f "./deploy-compose-$PROFILE.yml" ]; then
+        docker stack deploy -c - -c "./deploy-compose-$PROFILE.yml" $DEPLOY_STACK_NAME
+    else
+        docker stack deploy -c - $DEPLOY_STACK_NAME
+    fi
+}
+
